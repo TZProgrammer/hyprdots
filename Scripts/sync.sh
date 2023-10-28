fee9c066fdd8dc2a5028c8a523fa5821aaa35962
TZProgrammer <thomaszeger@gmail.com>
2023-10-27 23:43:49 -0400

    Create sync script

--- Scripts/sync.sh
+++ Scripts/sync.sh
@@ -1,6 +1,3 @@
 cp /home/thomas/.zshrc /home/thomas/hyprdots/Configs/.zshrc
 cp /home/thomas/.config/hypr/hyprland.conf /home/thomas/hyprdots/Configs/.config/hypr/hyprland.conf
 cp /home/thomas/.config/hypr/keybindings.conf /home/thomas/hyprdots/Configs/.config/hypr/keybindings.conf
-
-cd /home/thomas/hyprdots/Scripts/
-./install.sh
