6edc02a5ade194582edcf473bc124b181a8a2e5f
TZProgrammer <thomaszeger@gmail.com>
2023-10-27 21:53:35 -0400

    Fix update script

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,5 +1,6 @@
-cp /home/thomas/.zshrc /home/thomas/hyprdots/hyprdots/.zshrc
+cp /home/thomas/.zshrc /home/thomas/hyprdots/Configs/.zshrc
 cp /home/thomas/.config/hypr/hyprland.conf /home/thomas/hyprdots/Configs/.config/hypr/hyprland.conf
 cp /home/thomas/.config/hypr/keybindings.conf /home/thomas/hyprdots/Configs/.config/hypr/keybindings.conf
 
-./home/thomas/hyprdots/Scripts/install.sh
+cd /home/thomas/hyprdots/Scripts/
+./install.sh
