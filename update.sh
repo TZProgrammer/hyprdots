cc29b8a5b9c053aef59ac54b31c41536e8cb3619
TZProgrammer <thomaszeger@gmail.com>
2023-10-27 21:32:59 -0400

    Add autoupdatting script

---
+++ update.sh
@@ -0,0 +1,5 @@
+cp /home/thomas/.zshrc /home/thomas/hyprdots/hyprdots/.zshrc
+cp /home/thomas/.config/hypr/hyprland.conf /home/thomas/hyprdots/Configs/.config/hypr/hyprland.conf
+cp /home/thomas/.config/hypr/keybindings.conf /home/thomas/hyprdots/Configs/.config/hypr/keybindings.conf
+
+./home/thomas/hyprdots/Scripts/install.sh
